3ad5ffd618f8854c99f5d19f58bdc0f3596fbd0f
Ziyad-Y <zyacoubou@hawk.iit.edu>
2024-02-24 03:56:01 -0600

    .

--- Module/module.c
+++ Module/module.c
@@ -3,27 +3,27 @@
 #include <linux/module.h> 
   
 //MODULE LICENSE   
-MODULE_LICENSE("GPL")   
+MODULE_LICENSE("GPL");   
 
 // AUTHOR  
-MODULE_AUTHOR("Ziyad Yacoubou")  
+MODULE_AUTHOR("Ziyad Yacoubou");  
 
 // MODULE DESCRIPTION     
-MODULE_DESCRIPTION("My first module")  
+MODULE_DESCRIPTION("My first module");  
 
 // MODULE VERSION 
-MODULE_VERSION("0.1")
+MODULE_VERSION("0.1");
 
 static int __init start_mod(void){
-	printk("Loading my first module ..... \n"); 
-	printk("Z's Module\n");
+	printk(KERN_INFO "Loading my first module ..... \n"); 
+	printk(KERN_INFO "My Module\n");
 
 	return 0;  
 
 }
 
 static void __exit end_mod(void){
-	printk("Exiting first module\n");
+	printk(KERN_INFO "Exiting first module\n");
 }    
 
 
