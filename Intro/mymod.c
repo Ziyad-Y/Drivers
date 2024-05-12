f225b27cc4709763b8a3dd10a454f9288839c741
Ziyad-Y <ziyade300@gmail.com>
2024-05-12 14:21:15 -0500

    .

--- Intro/mymod.c
+++ Intro/mymod.c
@@ -13,7 +13,8 @@ MODULE_VERSION("0.1");
 
 
 //------------ declared types ---------------//   
-static char *name = "Unknown";   
+static char *first_name = "Unknown First"; 
+static char *last_name= "Unknown Last";  
 static int age = 0;   
 static int arr[3]={1,2,3};   
 static int arr_count = 0;
@@ -21,8 +22,11 @@ static int arr_count = 0;
 //--------- Single Types -----------------//
 module_param(age, int, S_IRUSR | S_IWUSR | S_IRGRP | S_IROTH);
 MODULE_PARM_DESC(age, "An integer"); 
-module_param(name, charp, 0000);   
-MODULE_PARM_DESC(name, "A character string");   
+module_param(first_name, charp, 0000);   
+MODULE_PARM_DESC(first_name, "A character string");   
+module_param(last_name, charp,0000);   
+MODULE_PARM_DESC(last_name, "A character string");
+
 //------- Array Types---------------------//
 module_param_array(arr, int, &arr_count, 0000);   
 MODULE_PARM_DESC(arr, "An array of integers");
@@ -30,7 +34,7 @@ MODULE_PARM_DESC(arr, "An array of integers");
 static int __init start_mod(void){
 	int i;	
 	pr_info("LOADING MODULE ......\n");   
-	pr_info("NAME : %s\n", name);   
+	pr_info("NAME : %s %s\n", first_name, last_name);   
 	pr_info("AGE : %d\n", age);   
 	for(i=0 ; i< ARRAY_SIZE(arr);i++)
 		pr_info("arr[%d]= %d\n", i, arr[i]);
