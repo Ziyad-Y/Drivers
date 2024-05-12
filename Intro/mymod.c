6b0fd99ae83429e308552422dbee8a8374f211e1
Ziyad-Y <ziyade300@gmail.com>
2024-05-12 13:44:52 -0500

    .

--- Intro/mymod.c
+++ Intro/mymod.c
@@ -4,7 +4,7 @@
 #include <linux/moduleparam.h>  
 #include <linux/stat.h>
 #include <linux/types.h>
-
+#include <linux/printk.h>
 //MODULE LICENSE   
 MODULE_LICENSE("GPL");   
 MODULE_AUTHOR("ME");      
@@ -12,30 +12,29 @@ MODULE_DESCRIPTION("My first module");
 MODULE_VERSION("0.1");
 
 static  int myage = 100;   
-static char myname[20] = "Random";
+static char * myname = "Random";
 static int arr[3]={0,1,2};   
-static int argc=0;
+static int argc = 0;
 //MODULE PARAMS
 //--------- Single Types -----------------//
 module_param(myage, int,S_IRUSR| S_IWUSR | S_IWGRP  | S_IRGRP);   
 MODULE_PARM_DESC(myage, "Age")     
-module_param(myname , charp , S_IRUSR| S_IRGRP | S_IROTH );
+module_param(myname , charp , 0000);
 MODULE_PARM_DESC(myname , "Name");
 //-------- Arrays -------------------------//
-module_param_array(arr, int, &argc, S_IRUSR|S_IRGRP | S_IROTH);   
+module_param_array(arr, int,&argc, 0000);   
 MODULE_PARM_DESC(arr, "INT ARRAY");
 
-static int data __initdata = 23;
 
 static int __init start_mod(void){
 	int i;	
-	printk(KERN_INFO "Loading my first module ..... \n"); 
-	printk(KERN_INFO "My Module %d \n ", data);
-	printk(KERN_INFO "Name is : %s\n", myname);
-	printk(KERN_INFO "I am %d years old \n", myage);      
-	for(i =0 ; i < 3 ; i++){
-		printk(KERN_INFO "myintArray[%d] = %d\n", i, arr[i]);;
+	pr_info("LOADING MODULE .... \n");
+	pr_info("NAME : %s\n", myname);
+	pr_info("I am %d years old \n", myage);      
+	for(i =0 ; i < ARRAY_SIZE(arr); i++){
+		pr_info( "myintArray[%d] = %d\n", i, arr[i]);;
 	}
+	pr_info("Got %d args in array", argc);
 	return 0;  
 
 }
