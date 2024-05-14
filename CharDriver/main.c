175e82b8382f816eaeeaa67ccb6738b9974b8494
Ziyad-Y <ziyade300@gmail.com>
2024-05-13 23:30:33 -0500

    .

--- CharDriver/main.c
+++ CharDriver/main.c
@@ -13,6 +13,15 @@ int main(void){
 		exit(-1);
 	}
 	printf("Sucess in opening /dev/mychardv\n");
+	char BUFF[100];
+	ssize_t bytes_read = read(fd, BUFF, 50);   
+	if(bytes_read ==0){
+		printf("No bytes read\n");    
+		return 0;
+	}
+	printf("Read: %s\n", BUFF);
+
+
 
 	return 0;
 }
