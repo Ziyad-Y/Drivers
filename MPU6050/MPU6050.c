572fae37bb04500f37969a863c509ee27c6a7730
Ziyad-Y <ziyade300@gmail.com>
2024-05-25 01:47:10 -0500

    .

--- MPU6050/MPU6050.c
+++ MPU6050/MPU6050.c
@@ -75,30 +75,30 @@ void read_from_MPU6050(struct i2c_client *mpu6050_client, s32 *data) {
     s32 tl, th;
     
     /* Reading Acceleration */
-    accelx_l = i2c_smbus_read_byte_data(mpu6050_client, ACCEL_XOUT_L_ADDR);
-    accelx_h = i2c_smbus_read_byte_data(mpu6050_client, ACCEL_XOUT_H_ADDR);
-    accely_l = i2c_smbus_read_byte_data(mpu6050_client, ACCEL_YOUT_L_ADDR);
-    accely_h = i2c_smbus_read_byte_data(mpu6050_client, ACCEL_YOUT_H_ADDR);
-    accelz_l = i2c_smbus_read_byte_data(mpu6050_client, ACCEL_ZOUT_L_ADDR);
-    accelz_h = i2c_smbus_read_byte_data(mpu6050_client, ACCEL_ZOUT_H_ADDR);
+    accelx_l = i2c_smbus_read_word_data(mpu6050_client, ACCEL_XOUT_L_ADDR);
+    accelx_h = i2c_smbus_read_word_data(mpu6050_client, ACCEL_XOUT_H_ADDR);
+    accely_l = i2c_smbus_read_word_data(mpu6050_client, ACCEL_YOUT_L_ADDR);
+    accely_h = i2c_smbus_read_word_data(mpu6050_client, ACCEL_YOUT_H_ADDR);
+    accelz_l = i2c_smbus_read_word_data(mpu6050_client, ACCEL_ZOUT_L_ADDR);
+    accelz_h = i2c_smbus_read_word_data(mpu6050_client, ACCEL_ZOUT_H_ADDR);
     data[0] = (accelx_h << 8) | accelx_l;
     data[1] = (accely_h << 8) | accely_l;
     data[2] = (accelz_h << 8) | accelz_l;
 
     /* Reading Gyroscope Data */
-    gyrox_l = i2c_smbus_read_byte_data(mpu6050_client, GYRO_XOUT_L_ADDR);
-    gyrox_h = i2c_smbus_read_byte_data(mpu6050_client, GYRO_XOUT_H_ADDR);
-    gyroy_l = i2c_smbus_read_byte_data(mpu6050_client, GYRO_YOUT_L_ADDR);
-    gyroy_h = i2c_smbus_read_byte_data(mpu6050_client, GYRO_YOUT_H_ADDR);
-    gyroz_l = i2c_smbus_read_byte_data(mpu6050_client, GYRO_ZOUT_L_ADDR);
-    gyroz_h = i2c_smbus_read_byte_data(mpu6050_client, GYRO_ZOUT_H_ADDR);
+    gyrox_l = i2c_smbus_read_word_data(mpu6050_client, GYRO_XOUT_L_ADDR);
+    gyrox_h = i2c_smbus_read_word_data(mpu6050_client, GYRO_XOUT_H_ADDR);
+    gyroy_l = i2c_smbus_read_word_data(mpu6050_client, GYRO_YOUT_L_ADDR);
+    gyroy_h = i2c_smbus_read_word_data(mpu6050_client, GYRO_YOUT_H_ADDR);
+    gyroz_l = i2c_smbus_read_word_data(mpu6050_client, GYRO_ZOUT_L_ADDR);
+    gyroz_h = i2c_smbus_read_word_data(mpu6050_client, GYRO_ZOUT_H_ADDR);
     data[3] = (gyrox_h << 8) | gyrox_l;
     data[4] = (gyroy_h << 8) | gyroy_l;
     data[5] = (gyroz_h << 8) | gyroz_l;
 
     /* TEMPERATURE */
-    tl = i2c_smbus_read_byte_data(mpu6050_client, TEMP_OUT_L_ADDR);
-    th = i2c_smbus_read_byte_data(mpu6050_client, TEMP_OUT_H_ADDR);
+    tl = i2c_smbus_read_word_data(mpu6050_client, TEMP_OUT_L_ADDR);
+    th = i2c_smbus_read_word_data(mpu6050_client, TEMP_OUT_H_ADDR);
     data[6] = (th << 8) | tl;
 }
 
