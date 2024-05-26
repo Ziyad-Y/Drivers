a18f521a1ff2af0782dfbdeee3f3a8252c4b6505
Ziyad-Y <ziyade300@gmail.com>
2024-05-26 18:11:10 -0500

    .

--- MPU6050/mpu.c
+++ MPU6050/mpu.c
@@ -27,39 +27,9 @@ static inline int32_t merge_bytes(int8_t high, int8_t low){
 	return (high << 8) | low;
 } 
 
-uint8_t buffer[4];
 
-int32_t read_data(int fd, uint8_t high_address, uint8_t low_address){
-	uint8_t low, high;
 
-	buffer[0] = high_address;
-	printf("Address being read 0x%x\n", buffer[0]);
-	if(write(fd, buffer, 1) != 1){
-		perror("Failed to write");
-		return -1;
-	}
 
-	if(read(fd, buffer, 2) != 2 ){
-		perror("Failed to read");
-		return -1;
-	}
-	
-	printf(" read 0x%x 0x%x\n",buffer[0],buffer[1] );   
-
-	buffer[0]=low_address;
-	printf("Address being read 0x%x\n", buffer[0]);
-	if(write(fd, buffer, 1) != 1){
-		perror("Failed to write");
-		return -1;
-	}
-	if(read(fd, buffer, 2) != 2){
-		perror("Failed to read");
-		return -1;
-	}
-	printf(" read 0x%x 0x%x\n",buffer[0],buffer[1] );  
-	return merge_bytes(high, low);
-
-}
 
 
 int main(void){
@@ -77,17 +47,23 @@ int main(void){
 		return -1;
 	}
 
-	uint32_t data[7];    
+	uint8_t data[7];   
 
-	data[0] = read_data(i2c_bus, ACCEL_XOUT_H_ADDR, ACCEL_XOUT_L_ADDR);   
-	data[1] = read_data(i2c_bus, ACCEL_YOUT_H_ADDR, ACCEL_YOUT_L_ADDR);  
-	data[2] = read_data(i2c_bus, ACCEL_ZOUT_H_ADDR, ACCEL_ZOUT_L_ADDR);   
-	data[3] = read_data(i2c_bus, GYRO_XOUT_H_ADDR, GYRO_XOUT_L_ADDR);   
-	data[4] = read_data(i2c_bus, GYRO_YOUT_H_ADDR, GYRO_YOUT_L_ADDR);   
-	data[5] = read_data(i2c_bus, GYRO_ZOUT_H_ADDR, GYRO_ZOUT_L_ADDR);
-	data[6] = read_data(i2c_bus, TEMP_OUT_H_ADDR, TEMP_OUT_L_ADDR);   
+	data[0]= ACCEL_XOUT_H_ADDR; 
+	if(write(fd, data, 1) != 1){
+		perror("Failed to write");
+		return -1;
+	}
 
-	printf(" accelx: 0x%x, gyrox 0x%x, temp 0x%x\n", data[0], data[3], data[6]);
+	if(read(fd, data, 2) != 2 ){
+		perror("Failed to read");
+		return -1;
+	}  
+
+	printf("data 0x%x 0x%x\n", data[0],data[1]);
+
+
+	
 
 
 
