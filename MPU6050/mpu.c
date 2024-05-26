39892ec372bac6a3925d46e95822d4383dae9227
Ziyad-Y <ziyade300@gmail.com>
2024-05-26 18:22:48 -0500

    .

--- MPU6050/mpu.c
+++ MPU6050/mpu.c
@@ -6,52 +6,47 @@
 #include <stdint.h>   
 #include <errno.h>
 
+#define MPU6050_ADDRESS 0x68
 #define ACCEL_XOUT_H_ADDR 0x3B
-#define ACCEL_YOUT_H_ADDR 0x3D
-#define ACCEL_ZOUT_H_ADDR 0x3F
 
-static inline int16_t merge_bytes(int8_t high, int8_t low){
-    return (high << 8) | low;
+static inline int16_t merge_bytes(uint8_t high, uint8_t low){
+    return (int16_t)((high << 8) | low);
 } 
 
 int main(void){
     int i2c_bus;   
-    int16_t accel_x, accel_y, accel_z;
+    int16_t accel_x;
     
     i2c_bus = open("/dev/i2c-1", O_RDWR);   
 
     if (i2c_bus < 0){
-        perror("failed to open bus");   
+        perror("Failed to open bus");   
         return -1;
     }
 
-    if (ioctl(i2c_bus, I2C_SLAVE, 0x68) < 0){
-        perror("error setting slave address");
+    if (ioctl(i2c_bus, I2C_SLAVE, MPU6050_ADDRESS) < 0){
+        perror("Error setting slave address");
         return -1;
     }
 
-    int8_t data[6]; // We need 6 bytes for X, Y, and Z axes
-
-    // Reading accelerometer data
-    data[0] = ACCEL_XOUT_H_ADDR; 
-    if (write(i2c_bus, data, 1) != 1){
-        perror("Failed to write");
+    uint8_t reg_addr = ACCEL_XOUT_H_ADDR;
+    if (write(i2c_bus, &reg_addr, sizeof(reg_addr)) != sizeof(reg_addr)){
+        perror("Failed to write register address");
         return -1;
     }
 
-    if (read(i2c_bus, data, 6) != 6){
-        perror("Failed to read");
+    uint8_t data[2]; // We need 2 bytes for X-axis
+
+    if (read(i2c_bus, data, sizeof(data)) != sizeof(data)){
+        perror("Failed to read data");
         return -1;
     }
 
-    // Merge high and low bytes for each axis
     accel_x = merge_bytes(data[0], data[1]);
-    accel_y = merge_bytes(data[2], data[3]);
-    accel_z = merge_bytes(data[4], data[5]);
 
-    for (int i = 0; i < 6; ++i) {
-    	printf("Byte %d: %02x\n", i, data[i]);
-	}
+    printf("Accelerometer data (X-axis): %d\n", accel_x);
+
+    close(i2c_bus);
 
     return 0;
 }
