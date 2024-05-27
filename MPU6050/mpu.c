5b21d2bb419ca4deee3bdbebe8e1c533ad233f41
Ziyad-Y <ziyade300@gmail.com>
2024-05-27 00:48:02 -0500

    .

--- MPU6050/mpu.c
+++ MPU6050/mpu.c
@@ -119,8 +119,9 @@ int main() {
 
 
 	while(FIFO_LEN != 1024){
-		
-		FIFO_LEN = merger_bytes(i2c_read(FIFO_COUNTL) , i2c_read(FIFO_COUNTH) );
+		ax_h = i2c_read(FIFO_COUNTH);
+		ax_l = i2c_read(FIFO_COUNTL);
+		FIFO_LEN = merger_bytes(ax_l ,ax_h );
 		printf("FIFO LEN = %d\n", FIFO_LEN);
 		if(FIFO_LEN == 1024){
 			i2c_write(USER_CTRL, 0x44);
