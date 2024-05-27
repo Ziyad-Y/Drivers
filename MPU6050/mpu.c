70bdc5b45dff57f3918dc0af7ea089cc2db797f5
Ziyad-Y <ziyade300@gmail.com>
2024-05-26 20:11:14 -0500

    .

--- MPU6050/mpu.c
+++ MPU6050/mpu.c
@@ -1,33 +1,57 @@
-#include <linux/i2c-dev.h>
-#include <fcntl.h>
-#include <unistd.h>
-#include <stdint.h>
-#include <stdlib.h>
 #include <stdio.h>
+#include <stdint.h>
+#include <unistd.h>
+#include <fcntl.h>
 #include <sys/ioctl.h>
+#include <linux/i2c-dev.h>
+#include <stdlib.h>
 
-int main(void){
-	int file;
-	char filename[20]="/dev/i2c-1";
-	int addr = 0x68; // I2C address of the MPU
-	uint16_t dataAddr = 0x0D; // Data address to read from
-	uint8_t val;
-	
-	
-	if ((file = open(filename, O_RDWR)) < 0)
-	    exit(1);
-	
-	if (ioctl(file, I2C_SLAVE, addr) < 0)
-	    exit(2);
-	
-	if (write(file, &dataAddr, 2) != 2) {
-	    exit(3);
-	}
-	
-	if (read(file, &val, 1) != 1) {
-	    exit(3);
-	}
-	// 'val' now contains the read data
-	printf("%d\n", val);
-	return 0;
+#define MPU6050_ADDRESS 0x68
+
+#define ACCEL_XOUT_H_ADDR 0x3B
+#define ACCEL_XOUT_L_ADDR 0x3C
+#define ACCEL_YOUT_H_ADDR 0x3D
+#define ACCEL_YOUT_L_ADDR 0x3E
+#define ACCEL_ZOUT_H_ADDR 0x3F
+#define ACCEL_ZOUT_L_ADDR 0x40
+
+#define TEMP_OUT_H_ADDR 0x41
+#define TEMP_OUT_L_ADDR 0x42
+
+#define GYRO_XOUT_H_ADDR 0x43
+#define GYRO_XOUT_L_ADDR 0x44
+#define GYRO_YOUT_H_ADDR 0x45
+#define GYRO_YOUT_L_ADDR 0x46
+#define GYRO_ZOUT_H_ADDR 0x47
+#define GYRO_ZOUT_L_ADDR 0x48
+
+int main() {
+	int fd;
+    
+
+    // Open the I2C bus
+    if ((fd = open("/dev/i2c-1", O_RDWR)) < 0) {
+        perror("Failed to open the bus.");
+        exit(1);
+    }
+
+    if (ioctl(fd, I2C_SLAVE, MPU6050_ADDRESS) < 0) {
+        perror("Failed to Access slave");
+        exit(1);
+    }
+    uint8_t buff[2]={0};   
+    uint8_t addr = 0x0D;
+    if(write(fd, &addr, 1)!=1){
+    	perror("Failed to write to device");
+    	exit(1);
+    }
+
+   if(read(fd, buff, 2)!=2){
+   	perror("Failed to read");
+   	exit(1);
+   }
+   printf("%d %d\n", buff[0],buff[1]);
+
+   close(fd);
+    return 0;
 }
