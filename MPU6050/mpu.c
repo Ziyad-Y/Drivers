619734fe3b1043d0968666225715378eb47a1b34
Ziyad-Y <ziyade300@gmail.com>
2024-05-26 19:34:07 -0500

    .

--- MPU6050/mpu.c
+++ MPU6050/mpu.c
@@ -4,8 +4,8 @@
 #include <fcntl.h>
 #include <sys/ioctl.h>
 #include <linux/i2c-dev.h>
+#include <stdlib.h>
 
-#define I2C_DEVICE "/dev/i2c-1"
 #define MPU6050_ADDRESS 0x68
 
 #define ACCEL_XOUT_H_ADDR 0x3B
@@ -26,45 +26,30 @@
 #define GYRO_ZOUT_L_ADDR 0x48
 
 int main() {
-    int file;
-    char filename[20];
-    int16_t data;
+	int fd;
+    
 
     // Open the I2C bus
-    sprintf(filename, "%s", I2C_DEVICE);
-    if ((file = open(filename, O_RDWR)) < 0) {
-        perror("Failed to open the i2c bus");
-        return 1;
+    if ((fd = open("/dev/i2c-1", O_RDWR)) < 0) {
+        perror("Failed to open the bus.");
+        exit(1);
     }
 
-    // Set the I2C slave address
     if (ioctl(file, I2C_SLAVE, MPU6050_ADDRESS) < 0) {
-        perror("Failed to acquire bus access and/or talk to slave");
-        return 1;
+        perror("Failed to Access slave");
+        exit(1);
     }
-
-    // Read accelerometer data
-    data = i2c_smbus_read_word_data(file, ACCEL_XOUT_H_ADDR);
-    printf("Accel X: %d\n", data);
-    data = i2c_smbus_read_word_data(file, ACCEL_YOUT_H_ADDR);
-    printf("Accel Y: %d\n", data);
-    data = i2c_smbus_read_word_data(file, ACCEL_ZOUT_H_ADDR);
-    printf("Accel Z: %d\n", data);
-
-    // Read temperature data
-    data = i2c_smbus_read_word_data(file, TEMP_OUT_H_ADDR);
-    printf("Temperature: %d\n", data);
-
-    // Read gyroscope data
-    data = i2c_smbus_read_word_data(file, GYRO_XOUT_H_ADDR);
-    printf("Gyro X: %d\n", data);
-    data = i2c_smbus_read_word_data(file, GYRO_YOUT_H_ADDR);
-    printf("Gyro Y: %d\n", data);
-    data = i2c_smbus_read_word_data(file, GYRO_ZOUT_H_ADDR);
-    printf("Gyro Z: %d\n", data);
-
-    // Close the I2C bus
-    close(file);
+    int8_t buff[1];   
+    buff[0] = 0x3B;   
+    if(write(fd, buff, 1)!=1){
+    	perror("Failed to write to device");
+    	exit(1);
+    }
+   if(read(fd, buff, 1)!=1){
+   	perror("Failed to read");
+   	exit(1)
+   }
+   printf("%d\n", buff[0]);
 
     return 0;
 }
