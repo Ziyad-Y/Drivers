8f53c34d03b97de24ebd3eee8cf260a19e1a1012
Ziyad-Y <ziyade300@gmail.com>
2024-05-26 21:23:50 -0500

    .

--- MPU6050/mpu.c
+++ MPU6050/mpu.c
@@ -8,6 +8,9 @@
 
 #define MPU6050_ADDRESS 0x68
 
+#define FS 0x1B  
+#define AFS 0x1C
+
 #define RESET_DEVICE_VAL 0x80  
 #define RESET_DEVICE_ADDR 0x6B
 
@@ -28,6 +31,26 @@
 #define GYRO_ZOUT_H_ADDR 0x47
 #define GYRO_ZOUT_L_ADDR 0x48
 
+static inline int16_t merge_bytes(uint8_t high, uint8_t low){
+	return (high << 8) | low;
+}
+
+uint16_t read_data(int fd,uint8_t h_addr){
+	uint8_t buff[2];   
+	buff[0] = h_addr;   
+	if(write(fd, buff, 1)!=1){
+		perror("Failed to write");
+		exit(1);
+	}
+	if(read(fd, buff, 2)!=2){
+		perror("Failed to read");
+		exit(1);
+	}
+
+	printf("%d %d", buff[0], buff[1]);   
+	return merge_bytes(buff[0], buff[1]);
+}
+
 int main() {
 	int fd;
    	uint8_t buff[2]; 
@@ -37,32 +60,41 @@ int main() {
         perror("Failed to open the bus.");
         exit(1);
     }
-
+    //check Device
     if (ioctl(fd, I2C_SLAVE, MPU6050_ADDRESS) < 0) {
         perror("Failed to Access slave");
         exit(1);
     }
+   //reset device
    buff[0] = RESET_DEVICE_ADDR;   
    buff[1] = RESET_DEVICE_VAL;
-
    if(write(fd, buff, 2)!=2){
    	perror("fail write");  
    	exit(1);
    } 
-   sleep(10);
-
-   buff[0]=ACCEL_XOUT_H_ADDR;
-   if(write(fd, buff, 1)!=1){
-   	perror("fail write");
-   	exit(1);
+   sleep(5);
+   //set FS  
+   buff[0]= FS;   
+   buff[1]= 0b00010000; //1000 deg/s
+   if(write(fd, buff,2)!=2){
+   	perror("Failed to write");
    }
 
-   if(read(fd, buff,2)!=2){
-   	perror("FAILED TO READ");
-   	exit(1);
+   //set AFS 
+   buff[0]= AFS;  
+   buff[1] = 0;
+   if(write(fd, buff,2)!=2){
+   	perror("Failed to write");
    }
 
-   printf("%d %d\n", buff[0],buff[1]);
+   sleep(5);
+   
+   uint16_t data[4];
+   data[0] = read_data(fd, ACCEL_XOUT_H_ADDR);   
+   data[1] = read_data(fd, ACCEL_YOUT_H_ADDR); 
+   data[2] = read_data(fd, ACCEL_ZOUT_H_ADDR);
+
+   printf("%d %d %d\n", data[0], data[1], data[2]);
 
     return 0;
 }
